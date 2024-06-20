8fadf7f4cd60122ec2122601553eb11f71c4e6bf
Scott Duclos <sduclos@gmail.com>
2024-06-20 07:09:12 -0400

    Return NextResponse from new route.ts

--- app/api/j/route.ts
+++ app/api/j/route.ts
@@ -2,4 +2,5 @@ import { type NextRequest } from 'next/server'
  
 export async function GET(request: NextRequest) {
     console.error("In the GET function")
+    return new Response("Hello from the GET function")
 }
