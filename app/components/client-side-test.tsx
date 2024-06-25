be223b9bf0fdca3eef8ccd9aabf3402afb5d9487
Matthew Brooker <matthewbrooker@Matthews-MacBook-Pro-2.local>
2024-06-25 07:29:00 -0400

    Don't compress the response

--- app/components/client-side-test.tsx
+++ app/components/client-side-test.tsx
@@ -1,44 +1,35 @@
 'use client'
-
 import { useState } from 'react';
-import axios from 'axios'; // Import the axios library
 
 export default function ClientSideTest() {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState('');
 
-  const handleClick = async () => {
-      const response2 = await axios.get('/api/j/hello', {responseType:"stream"});
-      setLoading(true);
-      const stream = response2.data;
-      console.log('getting the stream')
-      
-      if (!stream) return;
+  const handleClick = () => {
+    setLoading(true);
 
-      let result = '';
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '/api/j/hello', true);
+    xhr.responseType = 'text';
 
-      response2.data.on('data', (chunk:any) => {
-        // Process each chunk of data as it arrives
-        console.log(chunk.toString());
-      });
-  
-      response2.data.on('end', () => {
-        console.log('Stream ended');
-      });
+    xhr.onprogress = () => {
+      const response = xhr.responseText;
+      setData(response);
+    }
 
-      // while (true) {
-      //   console.log('reading...')
-
-      //   const { done, value } = await reader.read();
-      //   console.log('got a read')
-      //   if (done) break;
-      //   result += new TextDecoder().decode(value);
-      //   console.log(result)
-      //   setData(result);
-      // }
-      
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        setData(xhr.responseText);
+      }
       setLoading(false);
     };
+
+    xhr.onerror = () => {
+      setLoading(false);
+    };
+
+    xhr.send();
+  };
 
   return (
     <div>
